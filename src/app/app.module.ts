be68b80366a77f10e6abe60b516ec0fbd05cc038
Spiros <Spyros_Ponaris@hotmail.com>
2018-11-21 23:14:56 +0200

    1 commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailsComponent } from './hero-details/hero-details.component';
+import { MessagesComponent } from './messages/messages.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailsComponent,
+    MessagesComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/hero-details/hero-details.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Hero } from './../hero';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-details.component.html',
+  styleUrls: ['./hero-details.component.scss']
+})
+export class HeroDetailsComponent implements OnInit {
+
+  @Input() hero: Hero;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
